7e24177e621f39b1555597c6601d70b7c2216e12
Priyanshi <priyanshisaini2603@gmail.com>
2024-06-28 14:07:16 +0530

    Create column, images, and description

---
+++ main.py
@@ -0,0 +1,18 @@
+import streamlit as st
+
+st.set_page_config(layout='wide')
+
+col1, col2, = st.columns(2)
+
+with col1:
+    st.image("images/myphoto.jpg")
+
+with col2:
+    st.title("Priyanshi Saini")
+    content = """
+    Hi, I am Priyanshi! a dedicated data science student. I have a master's degree in data science.
+     Passionate about harnessing 
+    data to drive insights, I have developed projects leveraging Python’s capabilities in data analysis, visualization,
+     and machine learning
+    """
+    st.info(content)
